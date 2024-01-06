e6d1feb38bf9fb99aa8754b72cb967d906400134
MonsterLuni <monsterluni.specific@gmail.com>
2024-01-06 23:15:03 +0100

    first commit

---
+++ FuturArtXTrue/ExponatPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuturArtXTrue;
+
+public partial class ExponatPage : ContentPage
+{
+    public ExponatPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ FuturArtXTrue/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FuturArtXTrue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
